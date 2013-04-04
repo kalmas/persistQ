34eed71a9c5dc251d2ed56b5c4479b6453bc03f7
kalmas <kyle.almas@dominionenterprises.com>
2013-04-04 12:40:13 -0400

    added timed event func

--- public/js/event.js
+++ public/js/event.js
@@ -9,8 +9,8 @@
 		    	if(queue.peek() !== null){
 		    		sendEvent(queue);
 		    	} else {
-		    		window.console.log("queue is empty, sleep");
-		    		setTimeout(processEventQueue, 1000, queue);   
+		    		// window.console.log("queue is empty, sleep");
+		    		window.setTimeout(processEventQueue, 1000, queue);   
 		    	}
 		    };
 			
@@ -19,26 +19,36 @@
 		    	xhr.onreadystatechange = function(){
 		    		if(xhr.readyState == 4){
 		    			if(xhr.status == 200){
-		    				window.console.log("request succeeded, dequeue this event");
+		    				// window.console.log("request succeeded, dequeue this event");
 		    				queue.poll();
 		    			}
 		    			processEventQueue(queue);
 		    	    }
 		    	};
 		    	var event = queue.peek();
-		    	xhr.open("GET", "/ajax/eventLogging.ajax.php?eventid=" + event.eventid
-		    			+ "&someid=" + event.someid
-		    			+ "&typecode=" + event.typecode
-		    			+ "&pagename=" + event.pagename
-		    			+ "&source=" + event.source    			
-		    			, true);
+		    	if(typeof event.time === "undefined"){
+		    		// log common event
+		    		xhr.open("GET", "/ajax/eventLogging.ajax.php?eventid=" + event.eventid
+			    			+ "&someid=" + event.someid
+			    			+ "&typecode=" + event.typecode
+			    			+ "&pagename=" + event.pagename
+			    			+ "&source=" + event.source    			
+			    			, true);
+		    	} else {
+		    		// log timed event
+			    	xhr.open("GET", "/ajax/theaterLogging.ajax.php?siteid=" + event.siteid
+			    			+ "&code=" + event.code
+			    			+ "&time=" + event.time
+			    			+ "&length=" + event.length  			
+			    			, true);
+		    	}
 		    	xhr.send();
 		    };
 		    
 		    processEventQueue(eventLogQueue);
 		
 			/**
-			 * Global Event Log Functions
+			 * Global Common Log Event Functions
 			 */
 			window.common_log_event = function(eventid, someid, typecode, pagename, source){
 				var event = {};
@@ -47,7 +57,17 @@
 				event.typecode = typecode;
 				event.pagename = pagename;
 				event.source = source;
-				window.console.log("Queueing:" + window.JSON.stringify(event));
+				// window.console.log("Queueing:" + window.JSON.stringify(event));
+				eventLogQueue.offer(event);	
+			};
+			
+			window.common_log_timed_event = function(siteid, code, time, length){
+				var event = {};
+				event.siteid = siteid;
+				event.code = code;
+				event.time = time;
+				event.length = length;
+				// window.console.log("Queueing:" + window.JSON.stringify(event));
 				eventLogQueue.offer(event);	
 			};
 		}
