593c0bac75bf796f01c9688c515700cd7d5d9947
kalmas <kyle.almas@dominionenterprises.com>
2013-04-05 09:42:15 -0400

    some trivial validation

--- redis.js
+++ redis.js
@@ -5,8 +5,21 @@ client.on("error", function (err) {
     console.log("error event - " + client.host + ":" + client.port + " - " + err);
 });
 
-// Log event to text file
 app.get('/log-event', function(req, res){
+
+	// Quick Validate
+	if(typeof req.query.eventid !== 'string' 
+			|| typeof req.query.typecode !== 'string'
+			|| typeof req.query.someid !== 'string'
+			|| typeof req.query.sourcecode !== 'string'
+			|| typeof req.query.pagename !== 'string'
+			|| typeof req.query.url !== 'string'){
+		con
+		res.send(400, "Expected Params: eventid, typecode, someid, sourcecode, pagename, url");		
+		return;
+	}
+	
+	// Shape data the way the resque worker expects
 	var args = {};
 	args.event = req.query;
 	args.url = req.query.url;
@@ -19,6 +32,7 @@ app.get('/log-event', function(req, res){
 			}]
 	};
 	
+	// Push to redis
 	client.rpush("resque:queue:event", JSON.stringify(resqueObj), function(err, replies){
 		if(err){
 			res.send(500, err);
@@ -26,7 +40,6 @@ app.get('/log-event', function(req, res){
 			res.send(200);
 		}
 	});
-
 });
 
 app.listen(3000);
