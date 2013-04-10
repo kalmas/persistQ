78d9f0b969dfda26262d9290be5540b6f91b7c69
kalmas <kyle.almas@dominionenterprises.com>
2013-04-10 19:23:07 -0400

    added and removed some fields

--- redis.js
+++ redis.js
@@ -1,38 +1,43 @@
 var express = require('express'), app = express();
 var redis = require("redis"), client = redis.createClient(6379, 'redis.forrent.com');
 
+function isValid(req){
+	if(typeof req.query.eventid === 'string'
+			&& typeof req.query.someid === 'string'
+			&& typeof req.query.typecode === 'string'
+			&& typeof req.query.pagename === 'string'			
+			&& typeof req.query.sourcecode === 'string'){
+		return true;
+	} else if(req.query.siteid === 'string'
+			&& typeof req.query.code === 'string'
+			&& typeof req.query.time === 'string'			
+			&& typeof req.query.length === 'string'){
+		return true;
+	} else {
+		return false;
+	}
+}
+
 client.on("error", function (err) {
     console.log("error event - " + client.host + ":" + client.port + " - " + err);
 });
 
 app.get('/log-event', function(req, res){
 
-	// Quick Validate
-	if(typeof req.query.eventid !== 'string' 
-			|| typeof req.query.typecode !== 'string'
-			|| typeof req.query.someid !== 'string'
-			|| typeof req.query.sourcecode !== 'string'
-			|| typeof req.query.pagename !== 'string'
-			|| typeof req.query.url !== 'string'){
-		res.send(400, "Expected Params: eventid, typecode, someid, sourcecode, pagename, url");		
+	if(!isValid(req)){
+		res.send(400, "Expected Params: [eventid, typecode, someid, sourcecode, pagename, url] or [siteid, code, time, length]");		
 		return;
 	}
-	
-	// Shape data the way the resque worker expects
-	var args = {};
-	args.event = req.query;
-	args.url = req.query.url;
-	delete args.event.url;
+
 	var resqueObj = {
-			"class" : "QueueHandler\\EventLog",
+			"class" : "QueueHandler\\UserEventBuffer",
 			"args" : [{
-				"eventargs" : args.event,
-				"url" : args.url
+				"event" : req.query
 			}]
 	};
 	
 	// Push to redis
-	client.rpush("resque:queue:event", JSON.stringify(resqueObj), function(err, replies){
+	client.rpush("resque:queue:userEventBuffer", JSON.stringify(resqueObj), function(err, replies){
 		if(err){
 			res.send(500, err);
 		}else{
